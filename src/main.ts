7526abd8cb666c7218d6e4502d6995676ef70e3a
KevinYang <chgc_kevin@hotmail.com>
2016-05-29 00:16:43 +0800

    add firebase url memo

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ if (environment.production) {
 bootstrap(KeepthingsAppComponent,
   [
     FIREBASE_PROVIDERS,
-    defaultFirebase('https://keepthings.firebaseio.com'),
+    defaultFirebase('https://keepthings.firebaseio.com'),// can change to your firebase project url
     HTTP_PROVIDERS
   ]);
 
