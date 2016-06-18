e39edac974d33b16e6843e3113bea4d5531284d3
KevinYang <chgc_kevin@hotmail.com>
2016-06-18 22:16:13 +0800

    fix build for production bug

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,2 +1,3 @@
 export * from './environment';
 export * from './keepthings.component';
+export * from './shared';
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,18 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
-import { KeepthingsAppComponent, environment } from './app/';
+import { KeepthingsAppComponent, environment, AuthService } from './app/';
 import { HTTP_PROVIDERS } from '@angular/http';
 import { ROUTER_PROVIDERS }  from '@angular/router';
 import {FIREBASE_PROVIDERS,
   defaultFirebase,
   AngularFire } from 'angularfire2';
-import { AuthService } from './app/shared';
+
 
 if (environment.production) {
   enableProdMode();
 }
-
+enableProdMode();
+ 
 bootstrap(KeepthingsAppComponent,
   [
     FIREBASE_PROVIDERS,
